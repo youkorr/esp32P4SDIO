6ebe232d2a2ecd46f53d32e897ec26a5f7b80a3f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-22 01:31:56 +0200

    Update webdavbox3.h

--- components/webdavbox3/webdavbox3.h
+++ components/webdavbox3/webdavbox3.h
@@ -6,7 +6,7 @@
 #include <vector>
 #include "driver/sdmmc_host.h"
 #include "driver/sdmmc_defs.h"
-#include "../sd_mmc_card/sd_mmc_card.h"
+#include "../esp32p4_sdio/esp32p4_sdio.h"
 #include "esp_vfs_fat.h"
 #include "esp_netif.h"
 
