03cf2a0194f517bd169c8ad1be91a37e5329af12
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-22 01:02:46 +0200

    Update esp32p4_sdio.h

--- components/esp32p4_sdio/esp32p4_sdio.h
+++ components/esp32p4_sdio/esp32p4_sdio.h
@@ -9,6 +9,9 @@
 namespace esphome {
 namespace esp32p4_sdio {
 
+class ESP32P4SDIOTextSensor;
+class ESP32P4SDIOSensor;
+
 class ESP32P4SDIOComponent : public Component {
  public:
   void setup() override;
@@ -20,28 +23,38 @@ class ESP32P4SDIOComponent : public Component {
                 InternalGPIOPin *clk, InternalGPIOPin *cmd);
   void set_frequency(uint32_t frequency) { this->frequency_ = frequency; }
   void set_mount_point(const std::string &mount_point) { this->mount_point_ = mount_point; }
+  void set_slot(uint8_t slot) { this->slot_ = slot; }
   
   bool is_mounted() const { return this->mounted_; }
   size_t get_free_space();
   bool read_file(const std::string &path, std::string &content);
   bool write_file(const std::string &path, const std::string &content);
   std::string get_card_info();
+  
+  // Méthodes pour les capteurs
+  void add_text_sensor(ESP32P4SDIOTextSensor *sensor);
+  void add_sensor(ESP32P4SDIOSensor *sensor);
 
  protected:
-  InternalGPIOPin *dat0_pin_;
-  InternalGPIOPin *dat1_pin_;
-  InternalGPIOPin *dat2_pin_;
-  InternalGPIOPin *dat3_pin_;
-  InternalGPIOPin *clk_pin_;
-  InternalGPIOPin *cmd_pin_;
+  InternalGPIOPin *dat0_pin_{nullptr};
+  InternalGPIOPin *dat1_pin_{nullptr};
+  InternalGPIOPin *dat2_pin_{nullptr};
+  InternalGPIOPin *dat3_pin_{nullptr};
+  InternalGPIOPin *clk_pin_{nullptr};
+  InternalGPIOPin *cmd_pin_{nullptr};
   
   uint32_t frequency_{20000000};
   std::string mount_point_{"/sdcard"};
+  uint8_t slot_{1};
   bool mounted_{false};
   
-  sdmmc_card_t *card_;
+  sdmmc_card_t *card_{nullptr};
   sdmmc_host_t host_;
+  
+  std::vector<ESP32P4SDIOTextSensor *> text_sensors_;
+  std::vector<ESP32P4SDIOSensor *> sensors_;
 };
 
 }  // namespace esp32p4_sdio
 }  // namespace esphome
+
